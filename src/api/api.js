b8c16a7f102539e968b096e50b923f196b569b89
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2026-01-19 11:08:48 +0100

    added live url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://confession-api.vercel.app/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
